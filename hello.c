04c5b57d5a8dcbdd1a29a559b167588e0274ddbb
xiaotianxt <me@xiaotian.dev>
2025-10-05 14:00:54 -0400

    feat: 05

--- hello.c
+++ hello.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main ()  
-{
-  printf("Hello World \n");
+#include <unistd.h>
+#include <omp.h>
+
+int main() {
+    double A[10000];
+    omp_set_num_threads(4);
+    #pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        printf("%d\n", ID);
+    }
+    printf("All done\n");
 }
--- pi.c
+++ pi.c
@@ -2,9 +2,9 @@
 
 This program will numerically compute the integral of
 
-                  4/(1+x*x) 
-				  
-from 0 to 1.  The value of this integral is pi -- which 
+                  4/(1+x*x)
+
+from 0 to 1.  The value of this integral is pi -- which
 is great since it gives us an easy way to check the answer.
 
 The is the original sequential program.  It uses the timer
@@ -13,32 +13,42 @@ from the OpenMP runtime library
 History: Written by Tim Mattson, 11/99.
 
 */
-#include <stdio.h>
 #include <omp.h>
-static long num_steps = 100000000;
+#include <stdio.h>
+#define NUM_THREADS 8
+static long num_steps = 10000000000;
 double step;
-int main ()
-{
-	  int i;
-	  double x, pi, sum = 0.0;
-	  double start_time, run_time;
+int main() {
+  double x, pi, sum = 0.0;
+  double start_time, run_time;
 
-	  step = 1.0/(double) num_steps;
+  step = 1.0 / (double)num_steps;
 
-        	 
-	  start_time = omp_get_wtime();
+  start_time = omp_get_wtime();
 
-	  for (i=1;i<= num_steps; i++){
-		  x = (i-0.5)*step;
-		  sum = sum + 4.0/(1.0+x*x);
-	  }
+  double sums[NUM_THREADS];
+  long numBlocks = (num_steps + (NUM_THREADS - 1)) / NUM_THREADS;
+  printf("numBlocks: %ld", numBlocks);
 
-	  pi = step * sum;
-	  run_time = omp_get_wtime() - start_time;
-	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
-}	  
+  omp_set_num_threads(NUM_THREADS);
+#pragma omp parallel
+  {
+    int tid = omp_get_thread_num();
+    sums[tid] = 0;
+    printf("%d: from %ld to %ld\n", tid, numBlocks * tid,
+           numBlocks * (tid + 1));
+    for (long i = numBlocks * tid; i < num_steps && i < numBlocks * (tid + 1);
+         i++) {
+      double x = (i + 0.5) * step;
+      sums[tid] = sums[tid] + 4.0 / (1.0 + x * x);
+    }
+  }
+  for (int i = 0; i < 8; i++) {
+    sum += sums[i];
+  }
 
-
-
-
-
+  pi = step * sum;
+  run_time = omp_get_wtime() - start_time;
+  printf("\n pi with %ld steps is %lf in %lf seconds\n ", num_steps, pi,
+         run_time);
+}
