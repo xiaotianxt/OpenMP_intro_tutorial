cc02b3f1f39c7a79929aed4fbb668e291034ad9e
xiaotianxt <me@xiaotian.dev>
2025-10-05 14:31:26 -0400

    feat: improve using reduction and for

--- pi.c
+++ pi.c
@@ -15,7 +15,6 @@ History: Written by Tim Mattson, 11/99.
 */
 #include <omp.h>
 #include <stdio.h>
-#define NUM_THREADS 8
 static long num_steps = 10000000000;
 double step;
 int main() {
@@ -26,30 +25,11 @@ int main() {
 
   start_time = omp_get_wtime();
 
-  double sums[NUM_THREADS];
-
-  int nthreads;
-  omp_set_num_threads(NUM_THREADS);
-#pragma omp parallel
-  {
-    int tid = omp_get_thread_num();
-    double tsum = 0.0;
-    int threads = omp_get_num_threads();
-    long numBlocks = (num_steps + (threads - 1)) / threads;
-
-    for (long i = numBlocks * tid; i < num_steps && i < numBlocks * (tid + 1);
-         i++) {
-      double x = (i + 0.5) * step;
-      tsum += 4.0 / (1.0 + x * x);
-    }
-    if (tid == 0) {
-      nthreads = threads;
-      printf("Number of threads: %d\n", threads);
-    }
-#pragma omp atomic
-    sum += tsum;
+#pragma omp parallel for schedule(auto) reduction(+ : sum)
+  for (long i = 0; i < num_steps; i++) {
+    double x = (i + 0.5) * step;
+    sum += 4.0 / (1.0 + x * x);
   }
-
   pi = step * sum;
   run_time = omp_get_wtime() - start_time;
   printf("\n pi with %ld steps is %lf in %lf seconds\n ", num_steps, pi,
